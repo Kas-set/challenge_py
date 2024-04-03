0ea193457382fb84a90f3be614a41a41216fb4bf
Kas-set <111099372+Kas-set@users.noreply.github.com>
2024-04-03 11:42:05 +0000

    Delete mise a niveau directory

--- mise a niveau/collection.py
+++
@@ -1,2 +0,0 @@
-#tuples() : non mutables(inmutables)->non modifiable
-#listes []: mutables->modifiables 
--- mise a niveau/multiplication.py
+++
@@ -1,7 +0,0 @@
-def afficher_table_de_mult(n):
-    for i in range(11):
-        prod = i*n
-        
-        print(" ",i,"*",n,"= ",prod)
-        
-afficher_table_de_mult(4)
