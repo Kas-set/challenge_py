cb81d1927176d52361fe3732dabe84e15df7a215
Kas-set <111099372+Kas-set@users.noreply.github.com>
2024-04-04 14:13:14 +0000

    demande d'infos optimisé!

--- mise a niveau/poo2.py
+++ mise a niveau/poo2.py
@@ -1,11 +1,18 @@
 class Personne:
-    def __init__(self, nom):
+    def __init__(self, nom:str):
         self.nom = nom 
     def sePresenter(self):
         print(f"Salut je m'appelle {self.nom}")
+def demanderInfos():
+    while True:
+        try:
+            nbr = int(input("Combien de personnes seront inscrits?: "))
+            return nbr
+        except:
+            print("ERREUR:Saisir une valeur correcte!!!!!")
 
-nbr = int(input("Combien de personne seroent inscrit?: "))
 personnes = []
+nbr = demanderInfos()
 for i in range(nbr):
     nom = input(f"Saisir le nom de la personne {i+1}: ")
     personnes.append(Personne(nom))
