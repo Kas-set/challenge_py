e6108b6caf9b322872ff8f5d24ff095b69a112f9
Kas-set <111099372+Kas-set@users.noreply.github.com>
2024-04-05 14:58:23 +0000

    news

---
+++ mise a niveau/files.py
@@ -0,0 +1,13 @@
+#ouvrir (open) un fichier: nom du fichier, mode
+#ecrire (write): lire(read)
+#fermer(close)
+
+# f = open("mon_fichier.txt", "w")
+# f.write("Bonjour \n")
+# l = ["Premiere phrase", "deuxieme phrase"]
+# f.writelines(l)
+# f.close()
+# f = open("mon_fichier.txt", "r")
+# texte = f.read()
+# print(texte)
+# f.close()
